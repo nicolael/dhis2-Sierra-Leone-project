02928c333a8df576db986f4065fe791168403b52
Sara Haugse <Sara@1x-193-157-213-192.uio.no>
2016-11-16 14:28:01 +0100

    Polygons for level 2

--- app-skeleton-master/src/app/Map.js
+++ app-skeleton-master/src/app/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
+import { GoogleMapLoader, GoogleMap, Marker, Polygon } from 'react-google-maps';
 import Info from './Info';
 import { connect } from 'react-redux';
 import { clickedMarker } from '../actions';
@@ -36,6 +36,54 @@ class Map extends Component {
             }/>
         }
        });
+       //Polygon for level 2
+       var polyset = [];
+       let polygon2 = this.props.items.map((item, index) => {
+         //var s;
+         var s;
+         if(item.featureType=="MULTI_POLYGON" || item.featureType=="POLYGON") {
+           //ImspTQPwCqd = top parent id(Sierra Leone)
+           if(item.parent.id == "ImspTQPwCqd") {
+             //s is one long string of the polygon coordinates
+             s = item.coordinates.substr(4, item.coordinates.length - 6).split('],[');
+
+             var allPos = [];
+             var lastpos;
+             for(var i = 0; i < s.length; i++) {
+               var cord = s[i];
+               cord = cord.split(",");
+
+               var pos = {
+                 lat: parseFloat(cord[1]),
+                 lng: parseFloat(cord[0])
+               };
+               /*
+               if(lastpos != null) {
+                 var d = distance(lastpos.lat, lastpos.lng, pos.lat, pos.lng, 'K');
+
+                 if(d > 100) {
+                   allPos.push(pos);
+                   break;
+                 }
+               }
+               */
+               lastpos = pos;
+               allPos.push(pos);
+             }
+             polyset.push(
+               allPos);
+           }
+         }
+       });
+
+       var triangleCoords = [
+          {lat: 25.774, lng: -80.190},
+          {lat: 18.466, lng: -66.118},
+          {lat: 32.321, lng: -64.757},
+          {lat: 25.774, lng: -80.190}
+        ];
+
+        console.log("triangle: " + triangleCoords);
 
       return(
         <GoogleMapLoader
@@ -45,7 +93,8 @@ class Map extends Component {
               defaultZoom={8}
               defaultCenter={this.props.center}
               options={{streetViewControl: false, mapTypeControl: false}}>
-            {coordinates}
+              <Polygon paths = {polyset} />
+
             </GoogleMap>
 
           } />
@@ -53,3 +102,12 @@ class Map extends Component {
     }
 }
 export default connect(mapStateToProps) (Map);
+
+/*
+<Polygon
+strokeColor= "#000"
+path= {triangleCoords}
+key= {Date.now()}*/
+
+//{coordinates}
+//<Polygon paths = {polyset} />
