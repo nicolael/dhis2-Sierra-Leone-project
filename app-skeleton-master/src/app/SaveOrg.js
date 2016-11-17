0ee02bdf1a082bc8c3b8ebca0593f8d0a7c127f7
nicolael <nicolael@student.matnat.uio.no>
2016-11-17 10:33:37 +0100

    added SaveOrg component

---
+++ app-skeleton-master/src/app/SaveOrg.js
@@ -0,0 +1,26 @@
+import React, { Component, PropTypes } from 'react';
+import { saveOrganisationUnit } from '../api';
+
+class SaveOrg extends Component {
+
+
+	render(){
+
+		return( 
+	    <div>
+	    	<form>
+			  Name : 
+			  <input type="text" name="name"/> 
+			  ID :
+			  <input type="text" name="id" />
+			  Lat :
+			  <input type="text" name="lat" />
+			  Lng :
+			  <input type="text" name="lng" />
+			  <input type="submit" value="Submit" />
+			</form>
+        </div>
+        );
+	}
+}
+export default SaveOrg
