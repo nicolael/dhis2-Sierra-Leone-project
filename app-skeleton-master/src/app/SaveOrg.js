381e79ac581558cac96daf5cc8c09764a50913a9
nicolael <nicolael@student.matnat.uio.no>
2016-11-28 16:26:03 +0100

    new Working version of - Register organisation with parent id

--- app-skeleton-master/src/app/SaveOrg.js
+++ app-skeleton-master/src/app/SaveOrg.js
@@ -19,6 +19,9 @@ class SaveOrg extends Component {
     	featureType: 'POINT',
     	coordinates: '[-11.1447,10.4149]',
     	openingDate:'2016-01-01',
+      lat:'',
+      lng:'',
+      value:'',
       //Must fill out parent for it to show up
       parent: {
         id: 'DNRAeXT9IwS'
@@ -26,29 +29,27 @@ class SaveOrg extends Component {
     };
 
     this.handleName = this.handleName.bind(this);
-    this.handleId   = this.handleId.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.getCoords = this.getCoords.bind(this);
+    this.getLat = this.getLat.bind(this);
+    this.getLong = this.getLong.bind(this);
   }
 
   handleName(event) {
     this.setState({name: event.target.value});
+    this.setState({shortName: event.target.value})
   }
-  handleId(event) {
-    this.setState({id: event.target.value});
-  }
-  handleLat(event){
-  	this.setState({lat: event.target.value});
-  }
-  handleLng(event){
-  	this.setState({lng: event.target.value});
-  }
+
+
   handleSubmit(event) {
-    //this.setState({coordinates: '['+this.state.lng+","+this.state.lat});
-    alert('An org was submitted:\n'+"name : "+this.state.name+"\nlat : " +this.state.lat + "\nlong :"+this.state.lng+"\n coordinates : "+this.state.coordinates);
+
+    alert('An organisation unit was submitted\n'+"Name : "+this.state.name
+      +"\nParent : "+this.state.parent.id+"\nCoordinates : "+this.state.coordinates);
     event.preventDefault();
     saveOrganisationUnit(this.state);
   }
+  
   handleChange(event) {
     this.setState({parent: {id:event.target.value}});
     console.log(this.state.parent.id);
@@ -59,18 +60,20 @@ class SaveOrg extends Component {
         var MakeItem = function(X) {
               return <option>{X}</option>;
         };
-
         return <select id ={this.state.id} onChange={this.handleChange}>{Data.map(MakeItem)}</select>;
-    
     }
 
-  getLat(){
-    var test = this.props.coordState == null ? null : this.props.coordState.latLng.lat();
-    return test;
+  getLat(event){
+    console.log(this.state.lat);
   }
-  getLong(){
-    var test = this.props.coordState == null ? null : this.props.coordState.latLng.lng();
-    return test;
+  getLong(event){
+    console.log(this.state.lng);
+  }
+
+  getCoords(event){
+    var coords = "["+this.props.coordState.latLng.lat()+","+this.props.coordState.latLng.lng()+"]"
+    this.setState({coordinates : coords});
+    event.preventDefault();
   }
 
   render() {
@@ -81,6 +84,13 @@ class SaveOrg extends Component {
       <form onSubmit={this.handleSubmit}>
         Name:
         <input type="text" name={this.state.name} onChange={this.handleName} />
+        Lat:
+        <input type="text" value={this.props.coordState == null ? null : this.props.coordState.latLng.lat()} onChange={this.getLat}/>
+        Long:
+        <input type="text" value={this.props.coordState == null ? null : this.props.coordState.latLng.lng()} onChange={this.getLong}/>
+        <button onClick = {this.getCoords}>Set coordinates</button>
+        <br />
+        <br />
         Select parent :
         <label>
         {this.drop(this.props.parent)}
